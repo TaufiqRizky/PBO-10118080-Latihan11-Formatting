42c85a7e7ca4f173ed0cc3af942ae402e693345b
Taufiq R <taufiq.it2@gmail.com>
2019-09-24 20:39:26 +0700

    first commit

---
+++ src/pbo/pkg10118080/latihan11/formatting/PBO10118080Latihan11Formatting.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118080.latihan11.formatting;
+
+/**
+ *
+ * @author 
+ * Nama :Taufiq Rizky
+ * NIM  :10118080
+ * Deskripsi Program    : program menampilkan formatting
+ */
+public class PBO10118080Latihan11Formatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 3546764;
+        int iMinus =-i;
+        System.out.println("i : "+i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : "+f);
+        System.out.printf("%%f : %f%n",f);
+        System.out.printf("%%.2f : %.2f%n",f);
+        System.out.printf("%%12.2f : %12.2f%n",f);
+        System.out.printf("%%,12.2f : %,12.2f%n",f);
+    }
+    
+}
